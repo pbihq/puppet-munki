a23e0f88441550b53b02467d2dabdac9db3aab5a
Paul Galow <paul.galow@gmail.com>
2018-12-05 12:53:36 +0100

    Removing not needed functionality

--- files/auto_run.sh
+++ files/auto_run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Ugh, bash. What can I say, I need a bit of pain occasionally?
-
 /usr/local/munki/managedsoftwareupdate --auto
 /bin/rm /Library/LaunchDaemons/org.munki.auto_run.plist
 /bin/rm /usr/local/munki/auto_run.sh
--- manifests/init.pp
+++ manifests/init.pp
@@ -4,40 +4,14 @@
 # you must specify your own munki repo URL; please don't use http://munki and instead use a https URL.
 
 class munki (
-  String $munkitools_version,
-  Boolean $apple_software_updates_only,
-  String $client_cert_path,
-  String $client_identifier,
-  String $client_key_path,
-  Integer $days_between_notifications,
-  Boolean $install_apple_software_updates,
-  Boolean $unattended_apple_updates,
-  Integer $logging_level,
-  Boolean $log_to_syslog,
-  Boolean $msu_log_enabled,
-  String $software_repo_ca_cert,
   String $software_repo_url,
-  String $software_update_server_url,
-  Boolean $suppress_user_notification,
-  Boolean $use_client_cert,
-  Boolean $show_removal_detail,
   Integer $days_before_broken,
-  Array $additional_http_headers,
   String $payload_organization,
   String $package_source,
   Boolean $auto_run_after_install,
-  Boolean $perform_auth_restarts,
-  String $recovery_key_file,
-  Integer $use_notification_center_days,
-  Array $managed_installs,
-  Array $managed_uninstalls,
-  Boolean $show_optional_installs_for_higher_os_versions,
-  String $local_only_manifest_name,
 )
 {
-  class { '::munki::config': }
-  -> class { '::munki::install': }
+  class { '::munki::install': }
   -> class { '::munki::service': }
   -> Class['munki']
-
 }
