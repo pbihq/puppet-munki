80c5e01a08d5b993ad39025debbb33a5679bc90c
Paul Galow <paul.galow@gmail.com>
2018-12-05 14:11:04 +0100

    Fix: Missing String added to init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,7 @@
 # you must specify your own munki repo URL; please don't use http://munki and instead use a https URL.
 
 class munki (
+  String $munkitools_version,
   String $software_repo_url,
   Integer $days_before_broken,
   String $package_source,
